92424e004199cc1ec6649464f8140861e94918a7
Aiman <aimanaaw@gmail.com>
2020-02-21 15:05:10 +0000

    Conditional rendering

--- flaskblog.py
+++ flaskblog.py
@@ -2,14 +2,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [
+  {
+    'author': 'Jane Doe',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': 'April 21, 2018'
+  }
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+  # by adding additional arguments in the return render_template, we can pass additional data to the render file
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='about')
 
 if __name__ == '__main__':
     app.run(debug=True)
