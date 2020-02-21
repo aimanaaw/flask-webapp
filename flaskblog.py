ee775f804e50eee228e388029785c442cb3735ee
Aiman <aimanaaw@gmail.com>
2020-02-21 14:12:01 +0000

    Adding routes

--- flaskblog.py
+++ flaskblog.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return '<h1>Homepage</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About page</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True)
