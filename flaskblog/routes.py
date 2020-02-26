d12e0f6e5b4623962b25a9d06c3a1572262a014b
Aiman <aimanaaw@gmail.com>
2020-02-26 07:18:19 +0400

    Adding a profile picture to the user profile

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -75,4 +75,4 @@ def logout():
 @login_required
 def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account')
+    return render_template('account.html', title='Account', image_file=image_file)
