9acb38dd8ab94f1ca6c3d8ca2bba647082ac5bd8
Aiman <aimanaaw@gmail.com>
2020-02-27 06:22:44 +0400

    Used pillow to resize selected profile pictures to thumbnail sized

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -5,6 +5,7 @@ from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
 import secrets
 import os
+from PIL import Image
 
 posts = [
     {
@@ -78,7 +79,12 @@ def save_picture_function(form_picture):
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    form_picture.save(picture_path)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
     return picture_fn
 
 @app.route('/account', methods=['GET', 'POST'])
