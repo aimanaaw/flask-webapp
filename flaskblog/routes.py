7b92fffc3ce2071333626c5a3a706c3cdd481ddd
Aiman <aimanaaw@gmail.com>
2020-03-05 01:49:46 -0500

    Adding page numbers for user specific posts

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -142,10 +142,12 @@ def delete_post(post_id):
     return redirect(url_for('home'))
 
 
-@app.route('/user/<str:username>')
+@app.route('/user/<string:username>')
 def user_post(username):
     page = request.args.get('page', 1, type=int)
-    user = user.query.filter_by(username=username).first_or_404()
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
   # by adding additional arguments in the return render_template, we can pass additional data to the render file
-    return render_template('home.html', posts=posts)
+    return render_template('user_post.html', posts=posts, user=user)
